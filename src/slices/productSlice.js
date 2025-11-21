b01ef125c9ca85b2835c4a8b4808850a298cd8c0
Sara <sara.youseffi87@gmail.com>
2025-11-22 03:26:30 +0330

    commit url server

--- src/slices/productSlice.js
+++ src/slices/productSlice.js
@@ -2,39 +2,44 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-    items: [],
-    status: null,
+  items: [],
+  status: null,
 };
-const baseUrl= import.meta.env.MODE === "development" ? "http://localhost:10000/products" : "https://my-little-store-api.onrender.com/products";
+
+const baseUrl = import.meta.env.MODE === "development"
+  ? "http://localhost:9000/products"
+  : "https://my-little-store-api.onrender.com/products";
+
 export const fetchProducts = createAsyncThunk(
-    "products/fetchProducts",
-    async () => {
-        try {
-            const response = await axios.get( baseUrl);
-            return response.data;
-        } catch (err) {
-            console.log(err);
-        }
+  "products/fetchProducts",
+  async () => {
+    try {
+      const response = await axios.get(baseUrl);
+      return response.data || [];
+    } catch (err) {
+      console.error("Failed to fetch products:", err);
+      return []; // همیشه آرایه برمی‌گردانیم
     }
+  }
 );
 
 const productsSlice = createSlice({
-    name: "products",
-    initialState,
-    reducers: {},
-    extraReducers:builder=>{
-        builder
-            .addCase(fetchProducts.pending,(state,_)=>{
-                state.status="pending";
-            })
-            .addCase(fetchProducts.fulfilled,(state,action)=>{
-                state.items=action.payload;
-                state.status="success";
-            })
-            .addCase(fetchProducts.rejected,(state,action)=>{
-                state.status="rejected";
-            })
-    }
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.status = "pending";
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.items = action.payload;
+        state.status = "success";
+      })
+      .addCase(fetchProducts.rejected, (state) => {
+        state.status = "rejected";
+      });
+  },
 });
 
 export default productsSlice.reducer;
